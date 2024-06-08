d13b69c52bb33d7b27222098b00ea50650ead78c
breadworshipper <azhrayashna@gmail.com>
2024-06-08 12:03:59 +0700

    Chore: updated database credential

--- FM22_Player_Evaluation/settings.py
+++ FM22_Player_Evaluation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
         "USER": "postgres",
         "PORT": "5432",
-        "PASSWORD": "H59RCuyNQbRN@EV",
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
     }
 }
 
